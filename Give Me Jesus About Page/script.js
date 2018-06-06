904959bcd559254ebe1d0bc5d1bbed3d86a5d195
Aaron May <nogreatername@gmail.com>
2018-06-05 22:16:39 -0500

    update home page subheader

---
+++ Give Me Jesus About Page/script.js
@@ -0,0 +1,106 @@
+// var userName = prompt ("What is your name?");
+// alert ("Nice to meet you, " + userName);
+// console.log ( userName + ", it's great to see another developer. Surprise you found me.")
+// var firstName = prompt ("What is your first name?");
+// var lastName = prompt ("Nice to meet you, " + firstName + ". Can I also get your last name?");
+// var city = prompt ("What a lovely name you have, " + firstName + " " + lastName + ". Your husband is a lucky guy. May I ask what city you were born in?");
+// var age = prompt (city + " is a lovely area. I heard it snowed recently. I'm sure that was pretty, but very cold, am I right? Can I bother you for one more question? How old are you, " + firstName +"?");
+// alert ("So, " + firstName + " " + lastName + ", you are from " + city + ", you are " + age + " years old, and you are married to an amazing guy. He is really lucky to have you in his life. Thank you. Have a great day. And, by the way, I love you very much. <3"); 
+//console.log ("Only You Can Prevent Identity Theft!")
+
+
+//var age = prompt ("What is your age?");
+//if (age < 18) { alert ("I'm sorry. But you cannot come in.")
+//}else if (age < 21 ) { alert ("You may come in, but you may not drink.")
+//}else if(age == 21 ) { alert ("Happy 21st Birthday!!")
+//}else if (age % 2 != 0) { alert ("Your age is odd")
+//}else if(age % Math.sqrt(age) === 0) {alert ("Perfect Square!")
+//}else { alert ("Come on in!")};
+
+
+// Number Guessing Game
+
+// Create secretNumber
+//var secretNumber = 7;
+
+// Ask user for guess
+//var guess = prompt ("Pick a number between 1-10");
+
+// Check guess
+//if (Number (guess) === secretNumber) {
+//	alert("Congratulations!!")
+//}else if (Number (guess) > secretNumber) {prompt ("Too High. Please Try Again.")
+//}else if (Number(guess) < secretNumber) {prompt ("Too Low. Please Try Again.")};
+
+
+// While Loops
+//console.log ("Print all numbers between -10 and 19")
+//console.log("Printing all numbers")
+//var counter = -10;
+
+//while(counter < 20) {
+//	console.log(counter);
+//	counter++;
+//}
+
+//console.log("Print even numbers between 10 and 40")
+//var num = 10;
+//while(num <= 40) {
+//	console.log(num);
+//	num+=2;
+//}
+
+//console.log("Print all odd numbers between 300 and 333");
+//var num = 300;
+//while(num <= 333) {
+//	console.log(num);
+//	num+=3;
+//}
+
+//Annoy-O-Matic
+// var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah") {
+// 	var answer = prompt("Are we there yet");
+// }
+// alert("Yay!! We made it!!");
+
+//Annoy-O-Matic V2 - indexOf - lesson
+// var answer = prompt("Are we there yet?");
+
+// while(answer.indexOf("yes") === -1) {
+// 	var answer = prompt("Are we there yet");
+// }
+// alert("Yay!! We made it!!");
+
+
+//For Loops
+
+// console.log("Print all numbers between -10 and 19")
+// for(var i = -10; i <20; i++){
+// 	console.log(i)
+// }
+
+
+// console.log("Print all even numbers between 10 and 40")
+
+// for(var i = 10; i <=40; i+=1){
+// 	if(i % 2 === 0) {
+// 		console.log(i)}
+// 	}
+
+
+// console.log("Print all odd numbers from 300 to 333")
+
+// for(var i = 300; i <= 333; i+=1){
+// 	if(i % 2 !== 0) {
+// 		console.log(i);
+// 	}
+// }
+
+//Functions Lesson
+// function loveWhit() {
+// 	alert("I love you, Whit");
+// }
+
+// loveWhit();
